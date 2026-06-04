92b6cc48936b3c5c888511ded58c930d4baa6cc4
Eshaan Khan <esk@Eshaans-MacBook-Air.local>
2026-06-04 17:33:15 +0530

    Built fitness goal tracker

---
+++ fitness.js
@@ -0,0 +1,21 @@
+console.log("Eshaan's Fitness Tracker");
+let proteingoal=160;
+let watergoal=3000;
+let stepsgoal=10000;
+ let proteinintake=170;
+ let waterintake=2500;
+ let steps=12000;
+ if(proteinintake>=proteingoal)
+    console.log("Protein goal met for the day");
+    else{
+        console.log("take more Protein Eshaan");}
+ if(waterintake>=watergoal)
+    console.log("Water goal met for the day");
+    else{
+        console.log("drink more water Eshaan");
+    }
+ if(steps>=stepsgoal)
+    console.log("Steps goal met for the day");
+    else{
+        console.log("walk more Eshaan");
+ }
---
+++ index.js
@@ -0,0 +1,4 @@
+console.log(22 > 18);
+console.log(15 > 30);
+console.log(10 < 20);
+console.log(25 < 5);
